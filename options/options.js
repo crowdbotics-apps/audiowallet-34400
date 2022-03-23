eb9fab46dc587014b60742f555d9bf2b3237f728
Crowdbotics <team@crowdbotics.com>
2022-03-23 23:10:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "audiowallet_34400",
+  url: "https://audiowallet_34400.botics.co",
+  api: "https://audiowallet_34400.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
